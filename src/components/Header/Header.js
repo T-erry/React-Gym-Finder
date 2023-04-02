9b832919c76c56044485aa2a16be0c7c33ba8dd8
T-erry <terrywachuka10@gmail.com>
2023-04-03 01:41:26 +0300

    added a Heder component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+  return (
+    <header className='home-header'>
+    <h2>GymFinder</h2>
+    <p>Lift and Fit Gym</p>
+  </header>
+  )
+}
+
+export default Header
